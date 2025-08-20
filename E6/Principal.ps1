99e5d5d1cf16c39bc55b414cb6116c78eee52780
Bruno González <150694816+br1gzz@users.noreply.github.com>
2025-08-19 22:23:37 -0600

    Add files via upload

---
+++ E6/Principal.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host 
+
+Import-Module AuditoriaBasica 
+
+Write-Host "=== Auditoría básica de usuarios y servicios ===" 
+Write-Host "1. Mostrar usuarios inactivos" 
+Write-Host "2. Mostrar servicios externos activos" 
+
+$opcion = Read-Host "Selecciona una opción (1 o 2)" 
+ 
+switch ($opcion) { 
+    "1" { 
+        $usuarios = Obtener-UsuariosInactivos 
+        $usuarios | Format-Table Name, Enabled, LastLogon -AutoSize 
+        $usuarios | Export-Csv -Path "$env:USERPROFILE\Escritorio\users_inac.csv" -NoTypeInformation 
+        Write-Host "`n Reporte generado: ussers_inac.csv" 
+    } 
+    "2" { 
+        $servicios = Obtener-ServiciosExternos 
+        $servicios | Format-Table DisplayName, Status, StartType -AutoSize 
+        $servicios | ConvertTo-Html | Out-File "$env:USERPROFILE\Escritorio\serv_e.html" 
+        Write-Host "`n Reporte generado: serv_e.html" 
+    } 
+    default { 
+        Write-Host "Opción no válida." 
+    } 
+} 
